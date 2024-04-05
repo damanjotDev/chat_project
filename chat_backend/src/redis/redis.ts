087b2058d9e0715e5294818d1385d002a870f755
damanjot6767 <damanjot6767@gmail.com>
2024-04-05 13:38:29 +0530

    small changes

--- chat_backend/src/redis/redis.ts
+++ chat_backend/src/redis/redis.ts
@@ -2,11 +2,11 @@ import { Redis } from "ioredis";
 
 const redisInstance = new Redis({
 
-  port: 12305, // Redis port
-  host: "redis-12305.c14.us-east-1-3.ec2.cloud.redislabs.com", // Redis host
-  username: "default", // needs Redis >= 6
-  password: "DV17gwfQHIyzrAym7B8XtL4GFh995JcQ",
-  db: 0, // Defaults to 0
+  port: +process.env.REDIS_PORT, // Redis port
+  host: process.env.REDIS_HOST, // Redis host
+  username: process.env.REDIS_USERNAME, // needs Redis >= 6
+  password: process.env.REDIS_PASSWORD,
+  db: +process.env.REDIS_DB, // Defaults to 0
 
 })
 export { redisInstance }
